5ba27410fc2b4401046fbd956bc30975905ca948
Abdul Hadi <abdulhadi4172@gmail.com>
2025-05-25 19:42:54 +0545

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-const mobileBtn = document.getElementById("mobile-menu-button");
-const mobileMenu = document.getElementById("mobile-menu");
-
-mobileBtn.addEventListener("click", () => {
-    mobileMenu.classList.toggle("hidden");
-});
